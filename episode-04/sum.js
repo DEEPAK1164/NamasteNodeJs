49d7b6ae9809ec7ca4a8f19e99cbe974c285c373
deepak <2001dkmaurya@gmail.com>
2024-08-20 11:16:51 +0530

    some changes in episode-04

--- episode-04/sum.js
+++ episode-04/sum.js
@@ -1,27 +1,25 @@
 // note : modules protect their variables and functions from leaking variables
 //so we need to export the modules
-
 console.log("Sum module get exeuted");
-
 //x is defined in non strict mode which is allowed
 x="var x from sum module";
+
 function calSumFromSum(x,y){
     console.log(x+y);
 }
-
-
-
 // module.exports={
 //     calSumFromSum:calSumFromSum,
 //     x:x
 // };
 
 //OR
-
 module.exports={
     calSumFromSum,
     x,
 };
+
+
+
 
 //OR
 // console.log(module.exports) // => {}
