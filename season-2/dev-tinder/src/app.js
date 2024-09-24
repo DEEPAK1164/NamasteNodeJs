c9cb6192b3f17247ee24609f06cfd928fc08200b
deepak <2001dkmaurya@gmail.com>
2024-09-24 21:00:27 +0530

    episode-04 continue

--- season-2/dev-tinder/src/app.js
+++ season-2/dev-tinder/src/app.js
@@ -3,15 +3,27 @@ const app=express();
 
 
 //1st parameter is route handler 2nd cb fn below is known as request handler
-app.use('/',(req,res)=>{
-    res.send('Hello Hello Hello');
-    })
-    
 
+
+
+//this route handle every thing that comes after /hello not only /hello
 app.use('/test',(req,res)=>{
 res.send("Hello from the test route");
 })
 
+
+//this route handle every thing that comes after /xyz not only /xyz
+app.use('/xyz',(req,res)=>{
+    res.send("Hello from the xyz route");
+    })
+    
+    
+
+//this route handle every thing that comes after / not only / but order is very important
+app.use('/',(req,res)=>{
+        res.send('Hello Hello Hello');
+        })
+        
 
 
 app.listen(3000,()=>{
