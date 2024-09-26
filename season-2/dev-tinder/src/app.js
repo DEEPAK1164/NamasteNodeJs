42b2087fea2fdc339d6aa1e5ffef3eecc052425b
deepak <2001dkmaurya@gmail.com>
2024-09-26 23:17:38 +0530

    episode-05 continue

--- season-2/dev-tinder/src/app.js
+++ season-2/dev-tinder/src/app.js
@@ -1,68 +1,71 @@
 const express=require('express');
 const app=express();
-//Note : app.use() method will match all the HTTP method API calls to /test
-
-//1st parameter is route handler 2nd cb fn below is known as request handler
-
-//this route handle every thing that comes after /hello not only /hello
-// app.use('/test',(req,res)=>{
-// res.send("Hello from the test route");
-// })
 
 
-//this route handle every thing that comes after /xyz not only /xyz
-// app.use('/xyz',(req,res)=>{
-//     res.send("Hello from the xyz route");
-//     })
-    
-    
+// '/user'-> route, cb(req,res)-> route handler and
+// can have multiple route handler
 
-//this route handle every thing that comes after / not only / but order is very important
-// app.use('/',(req,res)=>{
-//         res.send('Hello Hello Hello');
-//         })
-        
+//................................................
+//coz 1st request handler does not give any response 
+//so it should second route handler but it is not like that
 
-// this will only handle GET call to /user
-// app.get('/user',(req,res)=>{
-//   res.send({firstName:"Deepak", lastName:"Maurya"})
-// })
+// app.use('/user', (req,res)=>{
+//    console.log('Handling the route handler');
+//   //  res.send("1st Response!!");
+// },
 
-// // this will only handle POST call to /user
-// app.post('/user',(req,res)=>{
-//    console.log("Save data to DB");
-//    res.send("Data Successfully saved to Database")
-//   })
+// (req,res)=>{
+//   console.log('Handling the route handler');
+//   res.send("2nd Response!!");
+// }
 
-app.get('/ab?c',(req,res)=>{
-  res.send("Ha Ha Ha Ha !!!!!!")
-})
+// )
+//...................................
 
-app.get('/pq+r',(req,res)=>{
-  res.send("Ha Ha Ha Ha !!!!!!")
-})
+//so, use next
+// app.use('/user', (req,res,next)=>{
+//   console.log('Handling the route handler');
+//    next();
+// },
 
+// (req,res)=>{
+//  console.log('Handling the route handler');
+//  res.send("2nd Response!!");
+// }
 
-app.get('/ab*cd',(req,res)=>{
-  res.send("Ha Ha Ha Ha !!!!!!")
-})
+// )
 
+//..................................................
+//Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
+// app.use('/user', (req,res,next)=>{
+//   console.log('Handling the route handler');
+//   res.send("1st Response!!");
+//    next();
+// },
 
-// if in any path route z comes it will work
-app.get(/z/,(req,res)=>{
-  res.send("Ha Ha Ha Ha !!!!!!")
-})
+// (req,res)=>{
+//  console.log('Handling the route handler');
+//  res.send("2nd Response!!");
+// }
 
-app.get('/user', (req,res)=>{
-  console.log(req.query);
-  res.send("Hello User !!!!!!")
-})
+// )
 
-app.get('/user/:id/:pwd', (req,res)=>{
-  console.log(req.params);
-  res.send("Hello User !!!!!!")
-})
+//...................................
+// Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are 
+app.use('/user', (req,res,next)=>{
+  console.log('Handling the route handler');
+  next();
+  res.send("1st Response!!");
+  
+},
 
+(req,res)=>{
+ console.log('Handling the route handler');
+ res.send("2nd Response!!");
+}
+
+)
+//...............................
 
 
 app.listen(3000,()=>{
