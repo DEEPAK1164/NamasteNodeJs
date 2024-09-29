61851513ff6c2963b1fdf9ea3e2b34d79a5de8fc
deepak <2001dkmaurya@gmail.com>
2024-09-29 15:42:51 +0530

    episode-05 done

--- season-2/episode-05/index3.js
+++ season-2/episode-05/index3.js
@@ -4,24 +4,19 @@ const app=express();
 app.get('/getUserDetails',(req,res)=>{
   //Logic of DB call and get user data and get some error
   //to deal with unhandeled errors
-  try{
   throw new Error("error occured!");
   res.send("user data send");
-  }
-  catch(err){
-    console.log(err?.message)
-    res.status(500).send("Some error contact support team!")
-  }
+
+
+
 })
 
 
 
 //order must be same like below
-//gracefully handling error to void ui break but best way is to use try catch
 app.use('/',(err,req,res,next)=>{
 if(err)
 {
-    //console.log(err);
     res.status(500).send("something went wrong!")
 }
 })
